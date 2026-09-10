1f550d20102a90f8a7fe9ce7fb9ae87182b6cdaf
Christopher Junk <christopher.junk@sap.com>
2026-09-10 13:57:40 +0200

    chore: add fips check
    On-behalf-of: @SAP christopher.junk@sap.com
    Signed-off-by: Christopher Junk <christopher.junk@sap.com>

--- cmd/service-provider-template/main.go
+++ cmd/service-provider-template/main.go
@@ -2,16 +2,19 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"os"
+
+	"github.com/openmcp-project/controller-utils/pkg/fips"
 
 	// opencontrolplane-gen:replace github.com/openmcp-project/service-provider-template=MODULE template=PROVIDER_NAME
 	"github.com/openmcp-project/service-provider-template/cmd/service-provider-template/app"
 )
 
 func main() {
+	fips.Verify(context.Background())
 	cmd := app.NewServiceServiceCommand()
-
 	if err := cmd.Execute(); err != nil {
 		fmt.Print(err)
 		os.Exit(1)
